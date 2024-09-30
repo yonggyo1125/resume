d9d3830da9f6980007900db88be50aaf78cc4343
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 12:01:13 +0900

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import './globals.css';
-
 export const metadata = {
   title: '이력서',
   description: '',
@@ -8,6 +7,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
