01f17086a4f9030837a40e678259c1afc988f91b
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 11:31:11 +0900

    .

--- src/app/main/containers/MainContainer.js
+++ src/app/main/containers/MainContainer.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { SectionsContainer, Section } from 'react-fullpage';
+
+const options = {
+  anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
+};
 
 const MainContainer = () => {
-  return <></>;
+  return (
+    <SectionsContainer {...options}>
+      <Section>
+        <h1>이력서</h1>
+      </Section>
+      <Section>
+        <h1>Education & Work Experience</h1>
+      </Section>
+      <Section>
+        <h1>Portfolio</h1>
+      </Section>
+    </SectionsContainer>
+  );
 };
 
 export default React.memo(MainContainer);
