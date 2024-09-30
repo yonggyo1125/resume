d3bca25d48206c827aee3947f537e268fe1402f8
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 15:25:53 +0900

    .

--- src/app/main/components/Experience.js
+++ src/app/main/components/Experience.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import { Section } from 'react-fullpage';
+import styled from 'styled-components';
+const Wrapper = styled.div`
+  &.first {
+    padding-top: 51px;
+  }
+
+  display: flex;
+  flex-wrap: wrap;
+
+  .item {
+    width: 0;
+    flex-grow: 1;
+    margin: 20px 0;
+  }
+
+  .item + .item {
+    margin-left: 20px;
+  }
+`;
 
 const Experience = () => {
   return (
