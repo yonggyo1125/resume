82fad5785b33777a87c70a8daa4f9cd05de9d7bd
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 14:14:44 +0900

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,11 @@ export default function RootLayout({ children }) {
     <html>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
           rel="stylesheet"
--- src/app/main/components/ProfileImage.js
+++ src/app/main/components/ProfileImage.js
@@ -2,13 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ProfileImage = ({ className }) => {
-  return <div className={className}></div>;
+  return (
+    <div className={className}>
+      <div className="layout-width">
+        <div className="tit">
+          <span>RESUME</span>
+          이이름
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const StyledProfileImage = styled(ProfileImage)`
   background: #bcbcbc;
   height: 350px;
   padding: 50px 30px 10px;
+  position: relative;
+
+  .tit {
+    position: absolute;
+    bottom: 10px;
+    font-size: 1.15rem;
+
+    span {
+      font-size: 1.75rem;
+      margin-right: 10px;
+    }
+  }
 `;
 
 export default React.memo(StyledProfileImage);
