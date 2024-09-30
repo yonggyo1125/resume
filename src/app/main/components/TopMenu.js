ba0261e47963c6e70c61e6bc005be75ec92c7be2
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 12:28:30 +0900

    .

--- src/app/main/components/TopMenu.js
+++ src/app/main/components/TopMenu.js
@@ -1,12 +1,26 @@
+'use client';
 import React from 'react';
 import { Header } from 'react-fullpage';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  background: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
 
 const TopMenu = () => {
   return (
     <Header>
-      <a href="#sectionOne">INFO & ABOUT</a>
-      <a href="#sectionTwo">EXPERIENCE</a>
-      <a href="#sectionThree">PORTFOLIO</a>
+      <StyledNav>
+        <a href="#sectionOne">INFO & ABOUT</a>
+        <a href="#sectionTwo">EXPERIENCE</a>
+        <a href="#sectionThree">PORTFOLIO</a>
+      </StyledNav>
     </Header>
   );
 };
