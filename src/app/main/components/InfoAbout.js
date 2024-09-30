cb3e41e5f9d7b3d95e6e10ec2a9ea6403b3e217a
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 14:38:33 +0900

    .

--- src/app/main/components/InfoAbout.js
+++ src/app/main/components/InfoAbout.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Section } from 'react-fullpage';
 import ProfileImage from './ProfileImage';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding-top: 51px;
+`;
 
 const InfoAbout = () => {
   return (
     <Section>
-      <ProfileImage />
-      <h1>이력서</h1>
+      <Wrapper>
+        <ProfileImage />
+        <div className="layout-width">
+          <h1>이력서</h1>
+        </div>
+      </Wrapper>
     </Section>
   );
 };
