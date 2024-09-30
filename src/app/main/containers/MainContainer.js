7a6ade269b6406344e608d77e46b4efb4e77e1a5
yonggyo1125 <yonggyo00@kakao.com>
2024-09-30 11:53:59 +0900

    .

--- src/app/main/containers/MainContainer.js
+++ src/app/main/containers/MainContainer.js
@@ -1,6 +1,10 @@
 'use client';
 import React from 'react';
-import { SectionsContainer, Section, Header } from 'react-fullpage';
+import { SectionsContainer } from 'react-fullpage';
+import TopMenu from '../components/TopMenu';
+import InfoAbout from '../components/InfoAbout';
+import Experience from '../components/Experience';
+import Portfolio from '../components/Portfolio';
 
 const options = {
   anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
@@ -9,10 +13,11 @@ const options = {
 const MainContainer = () => {
   return (
     <>
+      <TopMenu />
       <SectionsContainer {...options}>
-       
-        
-       
+        <InfoAbout />
+        <Experience />
+        <Portfolio />
       </SectionsContainer>
     </>
   );
